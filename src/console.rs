10b7af8e664231ea937e1df250764148e7d75d0e
Alice Cecile <alice.i.cecile@gmail.com>
2022-02-26 20:23:12 -0500

    Configure the console by default

--- src/console.rs
+++ src/console.rs
@@ -1,7 +1,7 @@
 //! Displays text in the console and accepts input
 
 use bevy::prelude::*;
-use bevy_console::ConsoleConfiguration;
+use bevy_console::{ConsoleConfiguration, ConsoleOpen};
 
 /// Controls the display of text on the console
 pub struct ConsolePlugin;
@@ -9,8 +9,25 @@ pub struct ConsolePlugin;
 impl Plugin for ConsolePlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(bevy_console::ConsolePlugin)
-            .insert_resource(ConsoleConfiguration {
-                ..Default::default()
-            });
+            .add_startup_system(configure_console);
     }
+}
+
+fn configure_console(
+    mut config: ResMut<ConsoleConfiguration>,
+    windows: Res<Windows>,
+    mut console_open: ResMut<ConsoleOpen>,
+) {
+    const COMMAND_LINE_HEIGHT: f32 = 30.;
+
+    // Match the size of the window
+    let window = windows.get_primary().unwrap();
+
+    config.width = window.width();
+    // Need a bit of space for the command line
+    config.height = window.height() - COMMAND_LINE_HEIGHT;
+
+    // The console is always is open
+    console_open.open = true;
+    config.keys = Vec::default();
 }
