1b2a75b06b5a29d70e094ff1975207e34ca1e753
Alice Cecile <alice.i.cecile@gmail.com>
2022-03-01 17:06:57 -0500

    Refactor to generalize Actions lifecycle

--- src/actions/attack.rs
+++ src/actions/attack.rs
@@ -1,5 +1,4 @@
 use crate::actions::Action;
-use crate::combat_flow::{CurrentAction, CurrentTurn};
 use bevy::prelude::*;
 use leafwing_terminal::*;
 
@@ -7,28 +6,10 @@ use leafwing_terminal::*;
 #[terminal_command(name = "attack")]
 pub(super) struct AttackCommand;
 
-/// Deals damage to the opponent, based on the active player
-pub(super) fn send_attack(
-    mut terminal_command: TerminalCommand<AttackCommand>,
-    current_turn: Res<CurrentTurn>,
-    mut current_action: ResMut<CurrentAction>,
-) {
-    // Break early if the command was not entered or was malformed
-    if terminal_command.take().is_none() {
-        return;
-    }
+#[derive(Debug, Default)]
+pub struct AttackAction;
 
-    // Break early if it is not time for input
-    match current_action.action() {
-        Some(_) => terminal_command.reply("You cannot use actions when another action is queued."),
-        None => current_action.set_action(Attack),
-    };
-}
-
-#[derive(Debug)]
-struct Attack;
-
-impl Action for Attack {
+impl Action for AttackAction {
     fn name(&self) -> String {
         "Attack".to_string()
     }
--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -1,17 +1,20 @@
 //! Actions that can be used by both players and monsters
 
+use crate::combat_flow::{CurrentAction, CurrentTurn};
+use bevy::ecs::system::Resource;
 use bevy::prelude::*;
 use core::fmt::Debug;
 use leafwing_terminal::*;
 
 mod attack;
+use attack::*;
 
 /// Adds [`TerminalCommands`](TerminalCommand) and [`Actions`](Action) for all of the available actions
 pub struct ActionPlugin;
 
 impl Plugin for ActionPlugin {
     fn build(&self, app: &mut App) {
-        app.add_terminal_command::<attack::AttackCommand, _, _>(attack::send_attack);
+        app.add_action::<AttackCommand, AttackAction>();
     }
 }
 
@@ -22,4 +25,36 @@ pub trait Action: Debug + Send + Sync + 'static {
 
     /// Runs the next system on the world
     fn advance(&self, world: &mut World);
+}
+
+trait Commandlike: Resource + CommandName + CommandArgs + CommandHelp {}
+
+impl<T: Resource + CommandName + CommandArgs + CommandHelp> Commandlike for T {}
+
+fn start_action<TC: Commandlike, A: Action + Default>(
+    mut terminal_command: TerminalCommand<TC>,
+    current_turn: Res<CurrentTurn>,
+    mut current_action: ResMut<CurrentAction>,
+) {
+    // Break early if the command was not entered or was malformed
+    if terminal_command.take().is_none() {
+        return;
+    }
+
+    match current_action.action() {
+        // Break early if it is not time for input
+        Some(_) => terminal_command.reply("You cannot use actions when another action is queued."),
+        // If everything is okay, set this action as the current action, from the beginning of its resolution
+        None => current_action.set_action(A::default()),
+    };
+}
+
+trait ActionExt {
+    fn add_action<TC: Commandlike, A: Action + Default>(&mut self);
+}
+
+impl ActionExt for App {
+    fn add_action<TC: Commandlike, A: Action + Default>(&mut self) {
+        self.add_terminal_command::<TC, _, _>(start_action::<TC, A>);
+    }
 }
